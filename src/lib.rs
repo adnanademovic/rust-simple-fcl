831d84f71d78eff90ef08ecfcb9870c758fe746b
Adnan Ademovic <adnanademovic100@gmail.com>
2018-01-12 14:01:31 +0100

    Make function interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,70 @@
 #![deny(warnings)]
 
-extern crate simple_fcl_sys as raw;
+pub extern crate nalgebra;
+pub extern crate simple_fcl_sys as raw;
+
+pub type Vec3f = nalgebra::Vector3<f32>;
+pub type Rotation3f = nalgebra::Rotation3<f32>;
+pub type Translation3f = nalgebra::Translation3<f32>;
+
+pub struct ModelBuilder {
+    model_ptr: raw::fcl_model_t,
+}
+
+impl Default for ModelBuilder {
+    fn default() -> Self {
+        Self {
+            model_ptr: unsafe {
+                let val = raw::fcl_model_new();
+                raw::fcl_model_begin(val);
+                val
+            },
+        }
+    }
+}
+
+impl ModelBuilder {
+    pub fn add_triangle(&mut self, p0: Vec3f, p1: Vec3f, p2: Vec3f) {
+        unsafe {
+            raw::fcl_model_add_triangle(
+                self.model_ptr,
+                p0.as_slice().as_ptr(),
+                p1.as_slice().as_ptr(),
+                p2.as_slice().as_ptr(),
+            )
+        }
+    }
+
+    pub fn build(self) -> Model {
+        unsafe {
+            raw::fcl_model_end(self.model_ptr);
+        }
+        Model {
+            model_ptr: self.model_ptr,
+        }
+    }
+}
+
+pub struct Model {
+    model_ptr: raw::fcl_model_t,
+}
+
+pub fn collide(
+    model_a: Model,
+    rotation_a: Rotation3f,
+    translation_a: Translation3f,
+    model_b: Model,
+    rotation_b: Rotation3f,
+    translation_b: Translation3f,
+) -> bool {
+    unsafe {
+        raw::fcl_collide(
+            model_a.model_ptr,
+            rotation_a.matrix().as_slice().as_ptr(),
+            translation_a.vector.as_slice().as_ptr(),
+            model_b.model_ptr,
+            rotation_b.matrix().as_slice().as_ptr(),
+            translation_b.vector.as_slice().as_ptr(),
+        ) != 0
+    }
+}
